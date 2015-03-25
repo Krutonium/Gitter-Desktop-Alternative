6d421ebe5191c6be2d8253fb272eaa227e50a0bd
Krutonium <pfckrutonium@gmail.com>
2015-03-25 18:52:12 -0400

    Inital Commit

---
+++ Gitter/Gitter/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGitter
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.GitterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GitterV01ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GitterNextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WB = New System.Windows.Forms.WebBrowser()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GitterToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(784, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'GitterToolStripMenuItem
+        '
+        Me.GitterToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GitterV01ToolStripMenuItem, Me.GitterNextToolStripMenuItem, Me.CloseToolStripMenuItem})
+        Me.GitterToolStripMenuItem.Name = "GitterToolStripMenuItem"
+        Me.GitterToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.GitterToolStripMenuItem.Text = "Gitter"
+        '
+        'GitterV01ToolStripMenuItem
+        '
+        Me.GitterV01ToolStripMenuItem.Name = "GitterV01ToolStripMenuItem"
+        Me.GitterV01ToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.GitterV01ToolStripMenuItem.Text = "Gitter v0.1"
+        '
+        'GitterNextToolStripMenuItem
+        '
+        Me.GitterNextToolStripMenuItem.Name = "GitterNextToolStripMenuItem"
+        Me.GitterNextToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.GitterNextToolStripMenuItem.Text = "Gitter Next"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'WB
+        '
+        Me.WB.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WB.Location = New System.Drawing.Point(0, 24)
+        Me.WB.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WB.Name = "WB"
+        Me.WB.Size = New System.Drawing.Size(784, 537)
+        Me.WB.TabIndex = 1
+        Me.WB.Url = New System.Uri("http://www.gitter.im/", System.UriKind.Absolute)
+        '
+        'frmGitter
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.WB)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmGitter"
+        Me.Text = "Gitter"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents GitterToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GitterV01ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GitterNextToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents WB As System.Windows.Forms.WebBrowser
+
+End Class
---
+++ Gitter/Gitter/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports Microsoft.Win32
+Imports System.IO
+Imports System.Net
+Imports System.Runtime.InteropServices
+
+Public Class frmGitter
+    Dim GitNext As Boolean
+    Dim time As DateTime
+    Private Sub GitterNextToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GitterNextToolStripMenuItem.Click
+        If GitNext = False Then
+            GitNext = True
+            My.Settings.GitterNext = True
+            Dim cook As New Cookie("gitter_staging=", "staged", "gitter.im", "/")
+            InternetSetCookie("https://www.gitter.com", Nothing, cook.ToString() + "; expires = " & time.ToUniversalTime().ToString("ddd, dd-MMM-yyyy HH:mm:ss") & " GMT")
+        Else
+            GitNext = False
+            My.Settings.GitterNext = False
+            Dim cook As New Cookie("gitter_staging", "none", "gitter.im", "/")
+            InternetSetCookie("https://www.gitter.com", Nothing, cook.ToString() + "; expires = " & time.ToUniversalTime().ToString("ddd, dd-MMM-yyyy HH:mm:ss") & " GMT")
+        End If
+        My.Settings.Save()
+        WB.Url = (New Uri("http://www.gitter.im/"))
+        WB.Navigate(WB.Url)
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        GitterV01ToolStripMenuItem.Enabled = False
+        Dim DisableQuirks As RegistryKey
+        DisableQuirks = Registry.CurrentUser.OpenSubKey("Software\Microsoft\Internet Explorer\Main\", True)
+        DisableQuirks.CreateSubKey("Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION")
+        DisableQuirks = Registry.CurrentUser.OpenSubKey("Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", True)
+        DisableQuirks.SetValue(Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location), 11000, RegistryValueKind.DWord)
+        DisableQuirks.SetValue(Path.GetFileName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName), 11000, RegistryValueKind.DWord)
+        'QUIRKS DISABLED!
+        Me.Height = My.Settings.SizeH
+        Me.Width = My.Settings.SizeW
+        GitNext = My.Settings.GitterNext
+    End Sub
+
+    Private Sub frmGitter_ResizeEnd(sender As Object, e As EventArgs) Handles Me.ResizeEnd
+        My.Settings.SizeH = Me.Height
+        My.Settings.SizeW = Me.Width
+        My.Settings.Save()
+    End Sub
+    <DllImport("wininet.dll", CharSet:=CharSet.Auto, SetLastError:=True)> _
+    Private Shared Function InternetSetCookie(lpszUrlName As String, lpszCookieName As String, lpszCookieData As String) As Boolean
+    End Function
+End Class
